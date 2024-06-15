41a8a60051244e2235e9c66098fb05f1a5adf473
xgian20 <162388975+xgian20@users.noreply.github.com>
2024-06-15 01:10:33 +0000

    saving, not completed yet

--- src/api/models.py
+++ src/api/models.py
@@ -3,17 +3,41 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user_table'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    invoices = db.relationship("Invoice" , back_populates="user")
 
     def __repr__(self):
         return f'<User {self.email}>'
-
+    
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Invoice(db.Model):
+     __tablename__ = 'invoice_table'
+     id = db.Column(db.Integer, primary_key=True)
+     invoice_date = db.Column(db.String(120), unique=False, nullable=False)
+     invoice_number = db.Column(db.String(120), unique=True, nullable=False)
+     invoice_amount = db.Column(db.Float, unique=False, nullable=False)
+     user_id = db.Column(db.ForeignKey('user_table.id'))
+     user = db.relationship("User", back_populates="invoices")
+
+def __repr__(self):
+    return f'<Invoice {self.invoice_number}>'
+    
+def serialize(self):
+    return {
+        "id": self.id,
+        "invoice_date": self.invoice_date,
+        "invoice_number": self.invoice_number,
+        "invoice_amount": self.invoice_amount,
+        "user_id": self.user_id,
+        # do not serialize the password, its a security breach
+    }
--- src/api/routes.py
+++ src/api/routes.py
@@ -6,17 +6,49 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+# token /POST
+# this route is for when the user ALREADY exists and needs an ACCESS token 
+# create user query with a conditional to see if user exists or return None
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+@api.route('/token', methods=['POST'])
+def generate_token():
+    # receiving request and converting body of 
+    # request into json format
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    
+    # query the user table to check if user exists
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user is None:
+        response = {
+            "msg": "Email or password does not match."
+        }
+        return jsonify(response), 401
+    
+    access_token = create_access_token(identity=user.id)
+    response = {
+        "access_token": access_token,
+        "user_id": user.id,
+        "msg": f'Welcome {user.email}! This worked!'
     }
+    
+    return jsonify(response), 200
 
-    return jsonify(response_body), 200
+# create a route for signup that will add the user's email and password to the database
+# POST 
+# test that on postman
+
+# create a route for /invoices that will retrieve and return the users invoices 
+# in json format
+# GET 
